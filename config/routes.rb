48f6e59a2da8024e1136afc3720dd9609153893a
Masud Khan <masudio@gmail.com>
2012-10-14 22:07:33 -0700

    end of ch5

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 SampleApp2::Application.routes.draw do
+  get "users/new"
+
+  match '/contact', :to => 'pages#contact' # automatically creates named routes (contact_path)
+  match '/about', :to => 'pages#about'
+  match '/help', :to => 'pages#help'
+  match '/signup', :to => 'users#new'
+
   get "pages/home"
 
   get "pages/contact"
@@ -54,7 +61,7 @@ SampleApp2::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "pages#home"
 
   # See how all your routes lay out with "rake routes"
 
